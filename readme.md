bcfe25cf70e89f083d0bf1bac1089b405bf1f53e
Ivan Varyuhin <varyuhin03@mail.ru>
2023-12-16 00:54:38 +0300

    add readme.md and fix main

---
+++ readme.md
@@ -0,0 +1,5 @@
+# noql-db
+
+Сборка:
+1. `cmake . -B build`
+2. `cmake --build build --target all`
--- src/main.c
+++ src/main.c
@@ -5,28 +5,26 @@
 
 int main(int argc, char **argv) {
 
-    if (argc > 1) {
-        int fd = open("file.bin", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    int fd = open("file.bin", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 
-        struct stat sb;
-        if (fstat(fd, &sb) == -1) {
-            printf("couldn't get file size\n");
-            return -1;
-        }
-
-        struct file *f;
-
-        if (sb.st_size == 0) {
-            f = init_file(fd);
-            printf("file created\n");
-        } else {
-            f = read_file(fd);
-            printf("file was read\n");
-        }
-
-        mega_test(f);
-
-        del_file(f);
+    struct stat sb;
+    if (fstat(fd, &sb) == -1) {
+        printf("couldn't get file size\n");
+        return -1;
     }
+
+    struct file *f;
+
+    if (sb.st_size == 0) {
+        f = init_file(fd);
+        printf("file created\n");
+    } else {
+        f = read_file(fd);
+        printf("file was read\n");
+    }
+
+    test_read(f);
+
+    del_file(f);
     return 0;
 }
